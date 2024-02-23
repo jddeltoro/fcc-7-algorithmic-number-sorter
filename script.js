13addc0b975f1bf0c66ffc0ff6b967a9cfb20153
jddeltoro <jdiazdeltoro@gmail.com>
2024-02-23 15:05:31 -0500

    Add logs to display how the sorting algorithm works

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const sortButton = document.getElementById("sort");
+const outputLog = document.getElementById("output-log");
 
 const sortInputArray = (event) => {
     event.preventDefault();
@@ -7,7 +8,8 @@ const sortInputArray = (event) => {
         ...document.getElementsByClassName("values-dropdown")
     ].map((dropdown) => Number(dropdown.value));
 
-    const sortedValues = selectionSort(inputValues);
+    outputLog.innerText = "";
+    const sortedValues = bubbleSort(inputValues);
 
     updateUI(sortedValues);
 }
@@ -20,8 +22,11 @@ const updateUI = (array = []) => {
 }
 
 const bubbleSort = (array) => {
+    let iteration = 0;
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length - 1; j++) {
+            iteration++;
+            updateLog(iteration, array, j);
             if (array[j] > array[j + 1]) {
                 const temp = array[j];
                 array[j] = array[j + 1];
@@ -51,6 +56,24 @@ const selectionSort = (array) => {
     return array;
 }
 
+const insertionSort = (array) => {
+    for (let i = 1; i < array.length; i++) {
+        const currValue = array[i];
+        let j = i - 1;
 
+        while (j >= 0 && array[j] > currValue) {
+            array[j + 1] = array[j];
+            j--;
+        }
+
+    }
+}
 
 sortButton.addEventListener("click", sortInputArray);
+
+function updateLog(iteration, array, j) {
+    console.log("Iteration: " + iteration, array, array[j], array[j + 1], `, Is ${array[j]} bigger than ${array[j + 1]}: ${array[j] > array[j + 1]}`);
+    const newParagraph = document.createElement("p");
+    newParagraph.innerText = `Iteration: ${iteration < 10 ? String(iteration).padStart(2,"0") : iteration} | ${String(array)}, ${array[j]}, ${array[j + 1]} | Is ${array[j]} bigger than ${array[j + 1]}: ${array[j] > array[j + 1]}`;
+    outputLog.appendChild(newParagraph);
+}
